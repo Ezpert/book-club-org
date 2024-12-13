4231341117cd4a36ebf517a602d7e814e0f8f395
Ravanex <alejandro.pugam@gmail.com>
2024-12-13 05:28:00 -0600

    made the code a bit more component based

--- frontend/src/src/Landing.tsx
+++ frontend/src/src/Landing.tsx
@@ -1,42 +1,66 @@
 import {useState} from "react";
 
 
+//so this syntax allows us to not have to do prop.whatever, it destructures it for us, that's why the component is declared so weirdly
+//if you have any questions about the code shoot me a message :) - Ruben
+const LandingPage = ({setPage}: {setPage: (page: string) => void}) =>{
+    return(
+        <div>
+            <button onClick={()=> setPage("bookClub")}>Go to Book Club</button>
+            <button onClick={() =>setPage("createClub")}>Create Book Club</button> 
+        </div>
+    )
+}
+
+const BookClub = ({setPage}: {setPage: (page: string) => void}) =>{
+    return(
+        <div>
+            <button onClick={() => setPage("landingPage")}>Go back to landing</button>
+            <p>Hello book clubbers! Weclome to the most optimal book club, BradleySexuals!</p>
+            <h1>Books being read right now:</h1>
+            <p>38 laws of power</p>
+            <p>psychology 101</p>
+        </div>
+    )
+}
+
+const CreateClub = ({setPage}: {setPage: (page: string) => void}) => {
+    return(
+        <div>
+            <p>here we can create clubs</p>
+            <button onClick={()=> setPage("landingPage")}>Go back to landing page</button>
+        </div>
+    )
+}
+
+
+
+
 const Landing = () =>{
 
-    const [page, setPage] = useState<string>("landing")
+    const [page, setPage] = useState<string>("landingPage")
 
 
-
-
-    return(
+    return (
         <>
+        {
+        
+            page === "landingPage"?(
+                <LandingPage setPage={setPage}/>
+            ): page === "bookClub" ?(
+                <BookClub setPage={setPage}/>
+            ): page === "createClub"? (
+                <CreateClub setPage={setPage}/>
+            ): (
+                <p>error page</p>
+            )
 
-
-            { page == 'landing' ?
-                ( <div>
-
-                    <p>Book clubs: </p>
-                    <button onClick={() => setPage("bookClub")}>Go To Book Club</button>
-
-
-                    <button onClick={() => setPage("createPage")}> Create book club</button>
-
-
-                </div>
-                )
-                :
-                (<div>
-                    <button onClick={()=> setPage("landing")}>Go back home!</button>
-                    <p>Hello book clubbers! Weclome to the most optimal book club, BradleySexuals!</p>
-                    <h1>Books being read:  </h1>
-                    <h4>38 laws of power</h4>
-                    <h5>Psychology 101</h5>
-
-                    </div>)
-
-            }
+        }
+        
         </>
     )
+
+
 
 }
 
